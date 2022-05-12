92ce7982baa45cda6efafc0295a753f963fec33d
pnagarsenker <pushkarnagarsenker@gamil.com>
2022-05-12 08:57:52 +0530

    removed the @autowire dependency

--- src/main/java/com/CamelRaceTrack/Controller/Repo.java
+++ src/main/java/com/CamelRaceTrack/Controller/Repo.java
@@ -2,7 +2,6 @@ package com.CamelRaceTrack.Controller;
 
 import com.CamelRaceTrack.Models.Camel;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
